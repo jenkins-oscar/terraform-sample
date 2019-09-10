1b8da033ad28f176bf03712123db472f831b7326
sharepointoscar <me@sharepointoscar.com>
2019-09-10 10:43:28 -0700

    adding tags

--- main.tf
+++ main.tf
@@ -7,4 +7,8 @@ resource "aws_instance" "my-linux-server" {
   ami           = "ami-08949fb6466dd2cf3"
   instance_type = "t2.micro"
 
+  tags = {
+    team = "jenkinsx"
+    event = "SPSENS"
+  }
 }
