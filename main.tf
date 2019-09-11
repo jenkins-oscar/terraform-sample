d823340828fddd61d5da42bcc34dd84af291eca0
sharepointoscar <me@sharepointoscar.com>
2019-09-11 14:16:54 -0700

    adding tags to instance

--- main.tf
+++ main.tf
@@ -7,4 +7,9 @@ resource "aws_instance" "my-linux-server" {
   ami           = "ami-08949fb6466dd2cf3"
   instance_type = "t2.micro"
 
+  tags = {
+          group = "Marketing"
+    cost-center = "COSTAF"
+  }
+
 }
